87193c5902dc4b34f9fb23b7dfc5e5b057087d95
Andre Rabelo <sepeticefet@gmail.com>
2019-06-06 19:35:00 -0300

    Commit Inicial 2

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor lkjalskdjflasj</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem oadfaufoiasdf oiu foiasdfkjsd</p>
+        <h2>Imprensa</h2>
+    </div>
+)
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import PageHeader from '../template/pageHeader'
+import TodoForm from '../todo/todoform'
+import TodoList from '../todo/todolist'
+
+const URL = 'http://localhost:3003/api/todos'
+
+export default class Todo extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            description : '',
+            list: []
+        }
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(e){
+        this.setState({...this.state, description : e.target.value })
+        
+    }
+    handleAdd(){
+        const description = this.setState.description
+        axios.post(URL, {description})
+            .then(resp => console.log("Funciona"))
+            .catch(erro => console.log(error.response))
+
+    }
+    
+    render(){
+        return (
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm 
+                    handleAdd={this.handleAdd}
+                    handleChange = {this.handleChange}
+                    description = {this.state.description}/>
+                <TodoList />
+            </div>
+        )
+    }
+}
